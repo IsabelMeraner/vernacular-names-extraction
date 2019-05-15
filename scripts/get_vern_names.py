13c0ed2ca369b965ccce6106430e571b93914341
IsabelMeraner <isabel.meraner@uzh.ch>
2019-05-15 16:45:56 +0200

    add extraction script.

---
+++ scripts/get_vern_names.py
@@ -0,0 +1,16 @@
+# usr/bin/env python3
+# author: Isabel Meraner
+# Project: Extraction of vernacular names from Bosshard (1978)
+# 2019
+
+"""
+Get vernacular names from Bosshard and associate them with scientific / German "booknames"
+in triple structure.
+"""
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
