a8c5f0f90c2d1f1b346c665c1c4548ee696acb6d
IsabelMeraner <isabel.meraner@uzh.ch>
2019-05-17 14:20:57 +0200

    add latin stopword filtering.

--- scripts/add_authorship_triples.py
+++ scripts/add_authorship_triples.py
@@ -12,7 +12,14 @@ $ python3 scripts/add_authorship_triples.py -i resources/bosshard_out_corrected.
 
 """
 import argparse
+from get_vern_names import _read_stoplist
 
+
+def _check_stopwords(vernacular_name, latin_stopwords):
+    if vernacular_name in latin_stopwords:
+        return True
+    else:
+        return False
 
 def main():
     argparser = argparse.ArgumentParser(description='Link found plant names to DB-entries in Catalogue of Life.')
@@ -35,16 +42,32 @@ def main():
         default='',
         help='pass author name')
 
+    argparser.add_argument(
+        '-l', '--latin',
+        type=str,
+        default='',
+        help='pass stoplist gazetteer to block latin names')
+
     args = argparser.parse_args()
     input_file = args.input_file
     author = args.author
     output_file = args.output_file
+    latin = args.latin
+    latin_stopwords = _read_stoplist(latin)
+
 
     with open(input_file, "r") as infile, open(output_file, "w", encoding="utf-8") as outfile:
+        triples_counter = 0
         for line in infile:
             print(line)
-            line = line.rstrip("\n")
-            outfile.write("{}\tuses_vernacular_name\t{}\n".format(author.upper(), line))
+            vernacular_name = line.rstrip("\n")
+            if _check_stopwords(vernacular_name, latin_stopwords):
+                continue
+            else:
+                triples_counter += 1
+                outfile.write("{}\tuses_vernacular_name\t{}\n".format(author.upper(), vernacular_name))
+
+        print("Extracted triples (unique): {}".format(triples_counter))
 
 
 if __name__ == '__main__':
