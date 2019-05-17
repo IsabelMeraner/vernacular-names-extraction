6523a7f074ef1d94dcbef20ce911338e5f9273db
IsabelMeraner <isabel.meraner@uzh.ch>
2019-05-17 11:33:14 +0200

    add extraction script.

---
+++ scripts/add_authorship_triples.py
@@ -0,0 +1,9 @@
+# usr/bin/env python3
+# author: Isabel Meraner
+# Project: Extraction of vernacular names from Bosshard (1978)
+# 2019
+
+"""
+Get vernacular names from Bosshard and associate them with scientific / German "booknames"
+in triple structure.
+"""
